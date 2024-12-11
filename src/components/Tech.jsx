5fb1f824ded132bc8ac95a7c2d992da83f4494ab
Ecmosplasmidou <cassamb97@gmail.>
2024-12-11 18:49:45 +0100

    test sans three drei

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,4 @@
-import { BallCanvas } from './canvas';
+// import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 import { motion } from "framer-motion";
@@ -19,7 +19,7 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10 mt-20'>
       {technologies.map((technology)=>(
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
+          {/* <BallCanvas icon={technology.icon}/> */}
           <p className='text-white-100 text-[14px] pl-1 tracking-wider text-center'>{technology.name}</p>
         </div>
       ))}
