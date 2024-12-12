cf5d27078ad568177fb3b24e46ee183940caad71
Ecmosplasmidou <cassamb97@gmail.>
2024-12-12 01:15:30 +0100

    modification tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,23 +8,24 @@ import { styles } from "../styles";
 const Tech = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={`${styles.sectionSubText} text-center`}>
-      The technologies I have learned so far
-      </p>
-      <h2 className={`${styles.sectionHeadText} text-center`}>
-        My Technologys
-      </h2>
-    </motion.div>
-    <div className='flex flex-row flex-wrap justify-center gap-10 mt-20'>
-      {technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-          <p className='text-white-100 text-[14px] pl-1 tracking-wider text-center'>{technology.name}</p>
-        </div>
-      ))}
-    </div>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          The technologies I have learned so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          My Technologies
+        </h2>
+      </motion.div>
+      <div className='flex flex-row flex-wrap justify-center gap-10 mt-20'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className='text-white-100 text-[14px] pl-1 tracking-wider text-center'>{technology.name}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
+
 export default SectionWrapper(Tech, "tech");
