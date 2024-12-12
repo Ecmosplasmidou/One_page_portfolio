e7ec0bd2e76aada34c0799bf43a0dd62e1ad8b5d
Ecmosplasmidou <cassamb97@gmail.>
2024-12-12 01:41:41 +0100

    maj

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
-        <div className="flex flex-col justify-center items-center">
+        <div className=" mt-[100px] flex flex-col justify-center items-center sml:mt-[150px]">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
           <div className="w-1 h-80 xs:h-100 sml:h-100 violet-gradient"></div>
         </div>
-        <div className="relative z-10">
+        <div className="sml:mt-[180px] mt-[150px] xs:mt-[80px] relative z-10">
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Cassam</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I'm a full-stack developer based in Toulouse, FR. <br className="sm:block hidden"/>
             I have developed many types of front-ends from well known Works applications to Ecommerce shops. <br className="sm:block hidden"/>
