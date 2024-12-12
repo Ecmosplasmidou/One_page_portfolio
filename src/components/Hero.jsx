13d8b4b37e43156d334456cb4f0e043c592b047f
Ecmosplasmidou <cassamb97@gmail.>
2024-12-12 01:37:33 +0100

    maj hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
-        <div className=" mt-[100px] flex flex-col justify-center items-center sml:mt-[150px]">
+        <div className="flex flex-col justify-center items-center">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
           <div className="w-1 h-80 xs:h-100 sml:h-100 violet-gradient"></div>
         </div>
